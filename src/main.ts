a8177879b72c9bcc38fe053a53f65eb4a0eaf19a
Matthias Arnoldy <m.arnoldy@outlook.de>
2026-04-12 12:41:36 +0200

    feat: add new SVG icons and styles for buttons and layout
    - Introduced new SVG icons: arrow-right, controller, and controllerBackground.
    - Created mixins for flexbox layout and absolute centering in SCSS.
    - Defined primary and secondary color variables.
    - Added font-face declarations for "Red Rose" and "Almarai" fonts.
    - Developed button styles with primary and secondary variants.
    - Implemented layout styles for the main index page and settings page.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import './styles/style.scss'
 
-init();
+document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const fieldRef = document.getElementById('field');
-    if (!fieldRef) return;
-    fieldRef.addEventListener("click", e => {
-        const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement;
-        if (card) {
-            card.classList.toggle("is-flipped");
-        }
-    })
+    
 }
